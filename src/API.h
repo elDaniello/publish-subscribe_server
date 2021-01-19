f80db3fc2a16661837d6b41d8dcb53e63d6d680f
Daniel <danielkotynski@gmail.com>
2021-01-20 00:06:54 +0100

    add API description for client

---
+++ src/API.h
@@ -0,0 +1,26 @@
+/*server responses*/
+#define CONNECTION_ESTABLILISHED "Connected\n"
+#define LOGIN_REQUEST "Enter your login\n"
+#define PASSWORD_REQUEST "Enter your password\n"
+#define LOGIN_SUCCESFULL "Logged in\n"
+#define BAD_LOGIN_CREDENTIALS "Login failed, try again\n"
+#define REGISTATION_SUCCES "Account created\n"
+#define REGISTATION_FAIL "Coudn't create account\n"
+#define TAG_CREATE_FAIL "Coudn't create tag\n"
+#define UNKNOWN_REQUEST "Unknown request\n"
+#define TAG_NAME_REQUEST "Enter tag name\n"
+#define ACCESS_DENIED "You don't have permission for this\n"
+#define CONNECTION_CLOSED "Disconnected\n"
+
+/*client requests*/
+#define REGISTER_REQUEST "Register\n"
+#define LOGIN_REQUEST "Login\n"
+#define CREATE_TAG "New tag\n"
+#define GET_TAGS_NAMES "Get tags\n"
+#define LOAD_TAG "Load tag\n"
+#define LOGOUT "logout\n"
+
+
+
+
+          
