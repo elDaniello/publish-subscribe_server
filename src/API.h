026ebfbea0eac3fb474969ca9f756e419fef1186
Daniel <danielkotynski@gmail.com>
2021-01-28 22:53:38 +0100

    add new server responses and user requests

--- src/API.h
+++ src/API.h
@@ -11,16 +11,21 @@
 #define TAG_NAME_REQUEST "Enter tag name\n"
 #define ACCESS_DENIED "You don't have permission for this\n"
 #define CONNECTION_CLOSED "Disconnected\n"
+#define LOGGED_OUT  "Logged out\n"
+#define SUBSCRIBTION_SUCCES "Subscribtion succesfull\n"
+#define SUBSCRIBTION_FAIL "Subscribtion failure\n"
+
 
 /*client requests*/
-#define REQUEST_MAX_LEN 10 /*size of the longest clien request*/
+#define REQUEST_MAX_LEN 11 /*size of the longest clien request*/
 #define REGISTER_REQUEST "Register\n"
 #define LOGIN_REQUEST "Login\n"
 #define CREATE_TAG "New tag\n"
 #define GET_TAGS_NAMES "Get tags\n"
 #define LOAD_TAG "Load tag\n"
-#define LOGOUT "logout\n"
-
+#define GET_FEED "Get feed\n"
+#define LOGOUT "Logout\n"
+#define SUBSCRIBE_TAG "Subscribe\n"
 
 
 
