abd213f12d9a6e2fdd4a1b4e81b8166306b39761
Daniel <danielkotynski@gmail.com>
2021-01-31 22:37:08 +0100

    change server error handling

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@
 
 #include "server.h"
 #include "API.h"
-
+#include "config.h"
 
 
 
@@ -234,9 +234,8 @@ void handleUser(int connection_socket_descriptor, struct USERS * users, struct T
     create_result = pthread_create(&thread1, NULL, ThreadBehavior, (void *)thread_data);
     handle_error(create_result);
     
-
     return;
-    //TODO (przy zadaniu 1) odbieranie -> wyświetlanie albo klawiatura -> wysyłanie
+    
 }
 
 int main(int argc, char* argv[])
@@ -245,16 +244,9 @@ int main(int argc, char* argv[])
 
     struct TAGS * tags = malloc(sizeof (struct TAGS));
     initTags(tags);
-    
 
     struct USERS * users =malloc(sizeof (struct USERS));
     loadUserData(users);
-    //test
-
-    createNewTag(tags, "tag testowy\n", "admin\n");
-    //newMessage(getTagStructByName(&tags, "tag testowy"), "admin", "hejo");
-    //newMessage(getTagStructByName(&tags, "tag testowy"), "admin", "hejo");
-
 
    int server_socket_descriptor;
    int connection_socket_descriptor;
@@ -263,49 +255,31 @@ int main(int argc, char* argv[])
    char reuse_addr_val = 1;
    struct sockaddr_in server_address;
 
-   //inicjalizacja gniazda serwera
    
    memset(&server_address, 0, sizeof(struct sockaddr));
    server_address.sin_family = AF_INET;
-   server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+   server_address.sin_addr.s_addr = inet_addr(SERVER_ADDR);
    server_address.sin_port = htons(SERVER_PORT);
 
    server_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
-   if (server_socket_descriptor < 0)
-   {
-       fprintf(stderr, "%s: Błąd przy próbie utworzenia gniazda..\n", argv[0]);
-       exit(1);
-   }
+   handle_error(server_socket_descriptor);
    setsockopt(server_socket_descriptor, SOL_SOCKET, SO_REUSEADDR, (char*)&reuse_addr_val, sizeof(reuse_addr_val));
 
    bind_result = bind(server_socket_descriptor, (struct sockaddr*)&server_address, sizeof(struct sockaddr));
-   if (bind_result < 0)
-   {
-       fprintf(stderr, "%s: Błąd przy próbie dowiązania adresu IP i numeru portu do gniazda.\n", argv[0]);
-       exit(1);
-   }
+   handle_error(bind_result);
 
    listen_result = listen(server_socket_descriptor, QUEUE_SIZE);
-   if (listen_result < 0) {
-       fprintf(stderr, "%s: Błąd przy próbie ustawienia wielkości kolejki.\n", argv[0]);
-       exit(1);
-   }
+   handle_error(listen_result);
 
    while(1)
    {
        connection_socket_descriptor = accept(server_socket_descriptor, NULL, NULL);
-       if (connection_socket_descriptor < 0)
-       {
-           fprintf(stderr, "%s: Błąd przy próbie utworzenia gniazda dla połączenia.\n", argv[0]);
-           exit(1);
-       }
-
+       handle_error(connection_socket_descriptor);
        handleUser(connection_socket_descriptor, users, tags);
    }
 
    close(server_socket_descriptor);
    free(tags);
    free(users);
-   //free(thread_data);
    return(0);
 }
--- src/server.h
+++ src/server.h
@@ -1,16 +1,6 @@
-#define SERVER_PORT 12345
-#define QUEUE_SIZE 5
-#define LOGIN_LEN 32
-#define PASSWD_LEN 32
-#define MAX_USR_COUNT 128
-#define MAX_TAGS_COUNT 64
-#define TAG_NAME_LEN 16
-#define MAX_MSG_LEN 280 //is that a twitter reference?
-
-
 #include <pthread.h>
 #include <stdbool.h>
-
+#include "config.h"
 
 
 #ifndef SERVER_H
@@ -122,6 +112,6 @@ int getUserSubscriptionsCount(struct TAGS tags, char * name);
 bool unsubscribe(struct TAGS * tags, char * tagName, char * user);
 
 //changes delimiter from newline to tab
-int replace_delimiter(char *str);
+void replace_delimiter(char *str);
 
 #endif
