47ddb4cb571ba70ec61f3f33af581cd1653261ee
Daniel <danielkotynski@gmail.com>
2021-01-31 22:36:37 +0100

    separate config macros to the config.h file

---
+++ src/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SERVER_PORT 1234
+#define SERVER_ADDR "127.0.0.1"
+#define QUEUE_SIZE 30
+#define LOGIN_LEN 32
+#define PASSWD_LEN 32
+#define MAX_USR_COUNT 128
+#define MAX_TAGS_COUNT 64
+#define TAG_NAME_LEN 16
+#define MAX_MSG_LEN 280 //is that a twitter reference?
+
+#endif
